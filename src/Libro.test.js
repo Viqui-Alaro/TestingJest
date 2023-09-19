5e909b169b01af1d02a1d6470e91917577a0984e
Viqui Maribel Alaro Mamani <viqui.alaro@s-nal-gtic-l124.ipa.impuestos.gob.bo>
2023-09-19 11:28:05 -0400

    Testing Jest

--- src/Libro.test.js
+++ src/Libro.test.js
@@ -8,6 +8,16 @@ describe('Libro', () => {
     expect(libroNuevo.calcularDescuento()).toBe(descuentoEsperado);
   });
 
+  it('debe calcular el descuento correctamente para un libro antiguo', () => {
+    const libroAntiguo = new Libro('Otro Título', 20, 5);
+    const descuentoEsperado = 4; // Descuento esperado para un libro antiguo del 20%
+    expect(libroAntiguo.calcularDescuento()).toBe(descuentoEsperado);
+  });
 
+  it('debe calcular el descuento correctamente para un libro con antigüedad intermedia', () => {
+    const libroIntermedio = new Libro('Libro Intermedio', 50, 3);
+    const descuentoEsperado = 7.5; // Descuento esperado para un libro con antigüedad intermedia del 15%
+    expect(libroIntermedio.calcularDescuento()).toBe(descuentoEsperado);
+  });
   
 });
